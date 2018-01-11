ba846dbdb6993a3697be955439407f816f3ed0d7
583760722 <583760722@qq.com>
2018-01-11 16:54:02 +0800

    the project body
    mybatis generator project

---
+++ mybatisGenerator/src/main/java/com/yh/mapper/OrdersMapper.java
@@ -0,0 +1,17 @@
+package com.yh.mapper;
+
+import com.yh.entity.Orders;
+
+public interface OrdersMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Orders record);
+
+    int insertSelective(Orders record);
+
+    Orders selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Orders record);
+
+    int updateByPrimaryKey(Orders record);
+}
